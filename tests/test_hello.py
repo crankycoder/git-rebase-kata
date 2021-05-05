ddb5eadda0856225275202a99214f26c6b982642
Victor Ng <victor@draper.ai>
2021-05-05 10:39:36 -0400

    [tests first!]  added tests

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -1,3 +1,8 @@
+import json
+
+from demo import app
+
+
 def test_index(client):
     response = client.get("/")
     assert response.data == b"Hello, World!"
@@ -5,11 +10,24 @@ def test_index(client):
 
 def test_new_handler(client):
     response = client.get("/fubar")
-    """
-    This should be:
-
-    import json
     assert json.loads(response.data)["status"] == "Situation fubar"
 
-    """
-    assert response.data == "Situation fubar"
+
+def test_some_crappy_database_write(client):
+    response = client.post(
+        "/posterboy",
+        data=json.dumps(dict(project="foo.bar",)),
+        content_type="application/json",
+    )
+
+    assert response.status_code == 200
+    # TODO: assert that the new row ID is in here
+    assert int(json.loads(response.data)["row_id"]) > 0
+    print(int(json.loads(response.data)["row_id"]))
+
+
+def test_get_connection():
+    # an undocumented testcase
+    memdb_fname = "file::memory:?cache=shared"
+    conn = app.create_connection(memdb_fname)
+    assert conn is not None
