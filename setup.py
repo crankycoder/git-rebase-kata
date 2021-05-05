346d22b0a1bb4880017b95f7221685d0a3d4232a
Victor Ng <victor@draper.ai>
2021-05-04 23:31:26 -0400

    init checkin

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="rebase-fu",
+    use_scm_version=False,
+    version="0.0.1",
+    setup_requires=["setuptools_scm", "pytest-runner"],
+    tests_require=["pytest"],
+    include_package_data=True,
+    packages=find_packages(exclude=["tests", "tests/*"]),
+    description="all about the rebase, no trouble",
+    author="VictorNg",
+    author_email="victor@crankycoder.com",
+    url="https://github.com/cranycoder/git-rebase-kata",
+    license="MPL 2.0",
+    install_requires=[],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+    ],
+    zip_safe=False,
+)
